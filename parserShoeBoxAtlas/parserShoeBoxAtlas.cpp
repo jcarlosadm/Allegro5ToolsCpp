4bda9e1a130c2eada3c588e90cd3cb051e0ad50c
carlos <carlosmaster10@gmail.com>
2014-06-07 14:10:24 -0300

    adicionando parser para arquivo do shoebox

---
+++ parserShoeBoxAtlas/parserShoeBoxAtlas.cpp
@@ -0,0 +1,198 @@
+#include "parserShoeBoxAtlas.h"
+
+// Para maiores detalhes de uso, veja parserShoeBoxAtlas.h
+
+// Configura todos os valores de currentSprite para -1, significando
+// que não há sprite atual com dados disponível
+void resetSpriteVal(spriteData *currentSprite){
+    currentSprite->posX=-1;
+    currentSprite->posY=-1;
+    currentSprite->w=-1;
+    currentSprite->h=-1;
+    currentSprite->offX=-1;
+    currentSprite->offY=-1;
+    currentSprite->origW=-1;
+    currentSprite->origH=-1;
+}
+
+////////////////////////////////////////////////////////////
+//              funções membros de Atlas                  //
+////////////////////////////////////////////////////////////
+
+Atlas::Atlas(){
+    // configura todos os valores de currentSprite para -1
+    resetSpriteVal(&currentSprite);
+}
+
+Atlas::Atlas(const char* fileName){
+    // configura todos os valores de currentSprite para -1
+    resetSpriteVal(&currentSprite);
+    // tenta abrir o arquivo solicitado
+    openFile(fileName);
+}
+
+Atlas::~Atlas(){
+    
+    // fecha atlasFile se estiver aberto
+    closeFile();
+    
+}
+
+void Atlas::openFile(const char* fileName){
+    
+    // abre o arquivo
+    atlasFile.open(fileName);
+    
+    // se houver algum problema (como o arquivo não existir)
+    if(!atlasFile.good()){
+        cout<<"problema ao abrir o arquivo!"<<endl;
+        
+        // fecha atlasFile se estiver aberto
+        closeFile();
+    }
+}
+
+void Atlas::closeFile(){
+    
+    // fecha o atlasFile se estiver aberto
+    if(atlasFile.is_open()){
+        atlasFile.close();
+    }
+    
+}
+
+int Atlas::getMetricAtlas(const char* type){
+    
+    // se o atlasFile não estiver aberto, retorna 0
+    if(!atlasFile.is_open())return 0;
+    
+    // aponta a leitura para o início do arquivo
+    atlasFile.seekg(0);
+    
+    string line; // guarda a linha
+    int value=0; // valor a ser retornado
+    
+    // pegar a primeira linha
+    getline(atlasFile,line);
+    
+    // se a métrica solicitada não existir, retorna 0
+    if(line.find(type)==string::npos)return 0;
+    
+    // a métrica foi encontrada
+    // encontra a posição da métrica
+    int index = line.find(type);
+    // encontra a posição da primeira abre aspas após a métrica
+    int index2 = line.find("\"",index+1);
+    // encontra a posição da primeira fecha aspas após a métrica
+    int index3 = line.find("\"",index2+1);
+    // pega a string entre as aspas
+    string numberstr = line.substr(index2+1,index3-index2-1);
+    // converte para inteiro
+    // deve usar a flag -std=c++0x no compilador
+    value = stoi(numberstr);
+    
+    // retorna a métrica solicitada
+    return value;
+}
+
+int Atlas::setSpriteData(const char* spriteName){
+    
+    // se o atlasFile não estiver aberto, retorna 0
+    if(!atlasFile.is_open())return 0;
+    
+    string line; // guarda a linha
+    // índices para procurar caracteres específicos
+    int index,index2,index3;
+    // guarda pedaços da linha
+    string valuestring;
+    // array de strings para procurar quando encontrar o sprite correto
+    string attributes[8]={"x=","y=","width=","height=","framex=","framey=","framew=","frameh="};
+    
+    // aponta a leitura para o início do arquivo
+    atlasFile.seekg(0);
+    
+    // enquanto houver linha...
+    while (getline(atlasFile,line)){
+    
+        // se achar a substring "SubTexture"...
+        if(line.find("SubTexture")!=string::npos){
+            
+            // Verifica o valor de "SubTexture"
+            // Se for igual a string fornecida para a função, então encontrou
+            // o sprite correto
+            index = line.find("SubTexture");
+            index2 = line.find("\"",index+1);
+            index3 = line.find("\"",index2+1);
+            valuestring.assign(line.substr(index2+1,index3-index2-1));
+            if(valuestring==spriteName){
+                
+                // percorre cada um dos oito atributos, procurando-os na linha
+                // atual.
+                for(int i=0;i<8;i++){
+                    index = line.find(attributes[i]);
+                    index2 = line.find("\"",index+1);
+                    index3 = line.find("\"",index2+1);
+                    valuestring.assign(line.substr(index2+1,index3-index2-1));
+                    
+                    // preenche currentSprite de acordo com o atributo encontado
+                    switch(i){
+                        case 0:
+                            currentSprite.posX = stoi(valuestring);
+                            break;
+                        case 1:
+                            currentSprite.posY = stoi(valuestring);
+                            break;
+                        case 2:
+                            currentSprite.w = stoi(valuestring);
+                            break;
+                        case 3:
+                            currentSprite.h = stoi(valuestring);
+                            break;
+                        case 4:
+                            currentSprite.offX = stoi(valuestring);
+                            break;
+                        case 5:
+                            currentSprite.offY = stoi(valuestring);
+                            break;
+                        case 6:
+                            currentSprite.origW = stoi(valuestring);
+                            break;
+                        case 7:
+                            currentSprite.origH = stoi(valuestring);
+                    }
+                }
+                // sai da função com sucesso
+                return 1;
+            }
+        }
+    
+    }
+    // não encontrou o sprite
+    return 0;
+}
+
+int Atlas::getSD(const char* attribute){
+    // se o atlas não estiver aberto, sai da função com falha
+    if(!atlasFile.is_open())return -1;
+    
+    if(!strcmp(attribute,"posX")){
+        return currentSprite.posX;
+    }else if(!strcmp(attribute,"posY")){
+        return currentSprite.posY;
+    }else if(!strcmp(attribute,"w")){
+        return currentSprite.w;
+    }else if(!strcmp(attribute,"h")){
+        return currentSprite.h;
+    }else if(!strcmp(attribute,"offX")){
+        return currentSprite.offX;
+    }else if(!strcmp(attribute,"offY")){
+        return currentSprite.offY;
+    }else if(!strcmp(attribute,"origW")){
+        return currentSprite.origW;
+    }else if(!strcmp(attribute,"origH")){
+        return currentSprite.origH;
+    }else{
+        // retorna -1 se não tiver sucesso
+        return -1;
+    }
+}
---
+++ parserShoeBoxAtlas/parserShoeBoxAtlas.h
@@ -0,0 +1,118 @@
+#ifndef READ_XML_TP
+#define READ_XML_TP
+
+// deve usar a flag -std=c++0x no compilador, para usar a função std::stoi(), que converte de string para inteiro
+
+#include <iostream> // remover depois
+#include <fstream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+// Estrutura dos dados de um único sprite
+struct spriteData{
+
+    // posição x no atlas
+    int posX;
+    
+    // posição y no atlas
+    int posY;
+    
+    // largura no atlas (não necessariamente da imagem original)
+    int w;
+    
+    // altura no atlas (não necessariamente da imagem original)
+    int h;
+    
+    // ajuste na posição x
+    int offX;
+    
+    // ajuste na posição y
+    int offY;
+    
+    // largura da imagem original
+    int origW;
+    
+    // altura da imagem original
+    int origH;
+};
+
+// Classe que contém informações do Atlas
+class Atlas{
+
+    private:
+    
+    // guarda o arquivo de atlas
+    ifstream atlasFile;
+    
+    // guarda dados de um sprite (para evitar grandes alocações, apenas um
+    // conjunto de dados é disponível por vez)
+    //
+    // Se algum dado de currentSprite for -1, isso quer dizer que não há dados
+    // de sprite carregados no momento
+    spriteData currentSprite;
+    
+    public:
+    
+    // construtor padrão
+    //
+    // inicializa todos os valores de currentSprite para -1
+    Atlas();
+    
+    // construtor
+    //
+    // tenta executar openFile()
+    // inicializa todos os valores de currentSprite para -1
+    //
+    // PARÂMETROS:
+    // fileName    : o nome do arquivo com extensão
+    //
+    // se o arquivo não existir, openFile() emitirá um alerta
+    Atlas(const char* fileName);
+    
+    // destrutor
+    // fecha o arquivo atlasFile
+    ~Atlas();
+    
+    // abre o arquivo de atlas em atlasFile manualmente
+    //
+    // PARÂMETROS
+    // fileName    : o nome do arquivo com extensão
+    //
+    // se o arquivo não existir, essa função emitirá um alerta
+    void openFile(const char* fileName);
+    
+    // fecha o arquivo atlasFile manualmente
+    void closeFile();
+    
+    // obtem métrica do Atlas (largura ou altura)
+    // retorna o valor da métrica solicitada
+    // se retornar 0, a métrica solicitada não foi encontrada
+    //
+    // PARÂMETROS:
+    // type    : string da métrica solicitada
+    //
+    // VALORES DE type
+    // Os valores para type podem ser: "width" ou "height"
+    int getMetricAtlas(const char* type);
+    
+    // Função que configura os dados do currentSprite de acordo com a string fornecida
+    // retorna 1 se achou o sprite, e 0 se ocorrer erro ou não encontrar o sprite
+    //
+    // PARÂMETROS:
+    // spriteName : nome do sprite, com extensão
+    int setSpriteData(const char* spriteName);
+    
+    // Função que retorna o valor de um dos atributos de currentSprite
+    // retorna -1 se não tiver sucesso, ou se o atlasFile estiver fechado
+    //
+    // PARÂMETROS:
+    // attribute : string do nome do atributo, idêntico a struct spriteData
+    //
+    // VALORES DE attribute
+    // os valores de attribute podem ser: "posX", "posY", "w", "h", "offX", "offY", "origW" ou "origH".
+    // (para mais informações sobre esses valores, veja a struct spriteData)
+    int getSD(const char* attribute);
+};
+
+#endif // READ_XML_TP
