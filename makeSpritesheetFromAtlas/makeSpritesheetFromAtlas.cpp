d076386f67fe900c513fe76182e2f817859bbc32
carlos <carlosmaster10@gmail.com>
2014-06-11 12:27:28 -0300

    comecando a definir o modulo makeSpritesheetFromAtlas

---
+++ makeSpritesheetFromAtlas/makeSpritesheetFromAtlas.cpp
@@ -0,0 +1,3 @@
+#include "makeSpritesheetFromAtlas.h"
+
+
---
+++ makeSpritesheetFromAtlas/makeSpritesheetFromAtlas.h
@@ -0,0 +1,22 @@
+#ifndef MAKE_SPRITESHEET_ATLAS_H
+#define MAKE_SPRITESHEET_ATLAS_H
+
+/** //////////////////////////////////////////////////////////////////////////
+Módulo que gera um spritesheet e todas as informações relacionadas, preenchendo
+um objeto da classe StatesClass (veja o módulo States).
+
+Veja makeSpritesheetFromAtlas.md para mais detalhes.
+////////////////////////////////////////////////////////////////////////////*/
+
+// inclusão do módulo States
+#include "States.h"
+// inclusão da biblioteca allegro
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+// inclusão das bibliotecas básicas
+#include <iostream>
+#include <string>
+
+
+
+#endif // MAKE_SPRITESHEET_ATLAS_H
